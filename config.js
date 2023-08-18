acf84158bebecfaae9c22473fae93b475f243c78
SHAVINDU <99857513+shavind@users.noreply.github.com>
2023-08-18 18:49:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -54,9 +54,9 @@ global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada 
 
 /*============== NOMOR ==============*/
 global.nomorbot = '68673045381' //Nomor Bot
-global.nomorown = '923470027813' //Nomor Owner
-global.namebot = 'Ikratos-MD-v1' //Nama Bot
-global.nameown = 'ᴀʜᴍᴀᴅ ᴀʟɪ🇵🇰' // Nama Owner
+global.nomorown = '923470027813' //shaviya bot
+global.namebot = 'Ikratos-MD-v1' //shaviya bot
+global.nameown = 'ᴀʜᴍᴀᴅ ᴀʟɪ🇵🇰' // shaviya bot
 
 
 /*============== CHATGPT ==============*/
